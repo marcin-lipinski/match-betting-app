bdef7f0712242851ae82b516687125d4ee9e3e48
marcin-lipinski <m.in.lipinski@gmail.com>
2023-04-24 17:22:42 +0200

    Styling UI, fetching data from API.

---
+++ src/main/java/pl/marcinlipinski/matchbettingapp/model/Match.java
@@ -0,0 +1,54 @@
+package pl.marcinlipinski.matchbettingapp.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@Setter
+public class Match {
+    @Id
+    private Integer id;
+    private String status;
+    private String homeTeam;
+    private String homeTeamLogo;
+    private String awayTeam;
+    private String awayTeamLogo;
+    private LocalDateTime startTime;
+    private Integer homeTeamScore;
+    private Integer awayTeamScore;
+    private Integer winnerCode;
+    private Double homeTeamOdd;
+    private Double drawTeamOdd;
+    private Double awayTeamOdd;
+    private String leagueName;
+    private String leagueLogo;
+
+
+    public Match(String gcc) {
+        super();
+        homeTeam = gcc;
+    }
+
+    @Override
+    public String toString(){
+        return id + status + homeTeam + awayTeam + homeTeamScore + awayTeamScore;
+    }
+
+    public String getHomeTeam(){
+        return homeTeam;
+    }
+}
